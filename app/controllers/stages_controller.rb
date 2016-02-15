bd1f1356f33921eb060281b62dd338b514740042
Roy <roybleas@ozemail.com.au>
2016-02-15 17:24:53 +1100

    add stage controller

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -13,11 +13,22 @@
 
 class StagesController < ApplicationController
   def index
+  	
+  	festival = Festival.current_active.take
+  	if festival.nil?
+  		@stages = []
+  		@days = 0
+  	else
+  		@days = festival.days
+  		@stages = Stage.current_active_festival.order(seq: :asc).all
+  	end
   end
 
   def show
   	stage_id = params[:id].to_i
   	dayindex = params[:dayindex].to_i
+  	
+  	Time.zone ="Sydney"
   	
   	festival = Festival.current_active.take
   	
@@ -26,6 +37,10 @@ class StagesController < ApplicationController
   	else
   		@dayindex = dayindex > festival.days ? festival.days : dayindex
   		@dayindex = 1 if dayindex < 1
+  		if !festival.startdate.nil?
+  			startdate = festival.startdate + (@dayindex - 1)
+  			@dayofweek = startdate.strftime("%a %d %b") 
+  		end
   	end
   	
   	@stage = Stage.current_active_festival.find_by_id(stage_id)
--- spec/controllers/stages_controller_spec.rb
+++ spec/controllers/stages_controller_spec.rb
@@ -20,6 +20,52 @@ RSpec.describe StagesController, :type => :controller do
 	      get :index
 	      expect(response).to have_http_status(:success)
 	    end
+	    it "assigns empty stages array when no stage records exist" do
+	    	get :index
+	    	expect(assigns(:stages)).to be_empty
+	    end
+	    it "assigns empty stage array when no festival record" do
+	    	get :index
+	    	expect(assigns(:stages)).to be_empty
+			end		
+	    it "assigns empty stage array when no records linked to festival record" do
+	    	get :index
+	    	expect(assigns(:stages)).to be_empty
+			end
+	    it "assigns a stage record linked to current festival to stage array" do
+	    	s = create(:stage_with_festival)
+	    	get :index
+	    	expect(assigns(:stages)).to match_array(s)
+			end
+			it "assigns multiple stage records to stage array" do
+				s = create(:stage_with_festival)
+				s1 = create(:stage, festival_id: s.festival_id, seq: 2)
+				get :index
+				expect(assigns(:stages)).to match_array([s,s1])
+			end
+			it "assigns multiple soreted stage records to stage array" do
+				s = create(:stage_with_festival, seq: 3)
+				s1 = create(:stage, festival_id: s.festival_id, seq: 1)
+				s2 = create(:stage, festival_id: s.festival_id, seq: 2)
+				get :index
+				expect(assigns(:stages)).to eq([s1,s2,s])
+			end
+			it "assigns 0 to days when no festival records" do
+				get :index
+				expect(assigns(:days)).to eq 0
+			end
+			it "assigns 0 to days when no current festival" do
+				festival = create(:festival_inactive)
+				get :index
+				expect(assigns(:days)).to eq 0
+			end
+			it "assigns festival.days to days for current festival" do
+				festival = create(:festival)
+				get :index
+				expect(assigns(:days)).to eq festival.days
+			end
+			
+
 	  end
 	end
 	context "show" do
@@ -58,6 +104,35 @@ RSpec.describe StagesController, :type => :controller do
 			it 'resets index to first if less than minimum' do
 				get :show, id: @stage, dayindex: 0
 				expect(assigns(:dayindex)).to eq 1
+			end
+		end
+		describe 'day of the week without festival record' do
+			it "retuns nil day of the week" do
+				get :show, id: 1, dayindex: 1
+				expect(assigns(:dayofweek)).to be_nil
+			end
+		end
+		describe 'actual date of day index' do			
+			before(:each) do
+				@festival = create(:festival,days: 3, startdate: '2016-03-1')
+				@stage = create(:stage, festival_id: @festival.id)
+			end				
+			it 'returns date of first day of festival' do
+				get :show, id: @stage, dayindex: 1
+				expect(assigns(:dayofweek)).to eq Date.parse('2016-03-01').strftime("%a %d %b")
+			end
+			it 'returns date of third day of festival' do
+				get :show, id: @stage, dayindex: 3
+				expect(assigns(:dayofweek)).to eq Date.parse('2016-03-03').strftime("%a %d %b")
+			end
+			it 'returns date of last day of festival when invalid day index' do
+				get :show, id: @stage, dayindex: 31
+				expect(assigns(:dayofweek)).to eq Date.parse('2016-03-03').strftime("%a %d %b")
+			end
+			it 'returns date of festival in following month with invalid day index' do
+				@festival.update(days: 40)
+				get :show, id: @stage, dayindex: 32
+				expect(assigns(:dayofweek)).to eq Date.parse('2016-04-01').strftime("%a %d %b")
 			end
 
 		end
