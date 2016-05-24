9f19964f607cad079ed981d32dc1e28884056be5
Roy <roybleas@ozemail.com.au>
2016-05-24 16:26:47 +1000

    Add readme and some comments

--- createreadmehtml.rb
+++
@@ -1,8 +0,0 @@
-require 'rdoc'
-
-data = File.read("README.rdoc")
-formatter = RDoc::Markup::ToHtml.new(RDoc::Options.new,nil)
-html = RDoc::Markdown.parse(data).accept(formatter)
-open('readme.html','w'){ |f|
-	f.puts html
-}
